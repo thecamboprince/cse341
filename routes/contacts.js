7a33c27dbb84f3386bc6538bd4d5d364fb6aaf90
thecamboprince <dug21002@byui.edu>
2024-01-26 12:03:02 -0700

    Give code comment

--- routes/contacts.js
+++ routes/contacts.js
@@ -11,10 +11,13 @@ router.get("/", contactsController.getAll);
 // Defining a route for handling the 'GET /:id' endpoint
 router.get("/:id", contactsController.getSingle);
 
+// Handle POST requests to create a new contact
 router.post("/", contactsController.createContact);
 
+// Handle PUT requests to update an existing contact by ID
 router.put("/:id", contactsController.updateContact);
 
+// Handle DELETE requests to delete a contact by ID
 router.delete("/:id", contactsController.deleteContact);
 
 // Exporting the router to be used in other parts of the application
